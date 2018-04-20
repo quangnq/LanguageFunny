535bc768cbdcc582df6116a6f4a19468c31a24bb
quang <quangnq.it@gmail.com>
2018-04-20 21:59:36 +0700

    hello

--- app/src/main/java/quangnq/co/myapplication/MainActivity.java
+++ app/src/main/java/quangnq/co/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // heelo
+        
         setContentView(R.layout.activity_main);
     }
 }
